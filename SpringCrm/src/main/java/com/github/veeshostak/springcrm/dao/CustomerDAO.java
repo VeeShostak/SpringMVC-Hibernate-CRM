5b1c790dc726fac48a7b7f1cadc7aa450898d02e
VeeShostak <VeeShostak@users.noreply.github.com>
2017-10-06 22:42:12 -0400

    convert project from Dynamic Web project to Maven project. initial

---
+++ SpringCrm/src/main/java/com/github/veeshostak/springcrm/dao/CustomerDAO.java
@@ -0,0 +1,11 @@
+package com.github.veeshostak.springcrm.dao;
+
+import java.util.List;
+
+import com.github.veeshostak.springcrm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+	
+}
---
+++ SpringCrm/src/main/java/com/github/veeshostak/springcrm/dao/CustomerDAOImpl.java
@@ -0,0 +1,44 @@
+package com.github.veeshostak.springcrm.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.veeshostak.springcrm.entity.Customer;
+
+// @repository required for DAO
+// component scan will find  the repository, handle exception translation
+@Repository 
+public class CustomerDAOImpl implements CustomerDAO {
+
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+			
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		// get the current hibernate session (Note: session factory was injected using @autowired)
+		Session currentSession = sessionFactory.getCurrentSession();
+
+		// create a query
+		Query<Customer> theQuery = 
+				currentSession.createQuery("from Customer", Customer.class);
+		
+		// execute query and get list
+		List<Customer> customers = theQuery.getResultList();
+				
+		// return the results		
+		return customers;
+	}
+
+}
+
+
+
